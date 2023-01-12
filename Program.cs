54af8604226f40143577fa73020e792b6c31add1
Sangam Rimal <sangamr.official@gmail.com>
2023-01-12 20:03:04 +0545

    fix

--- Program.cs
+++ Program.cs
@@ -3,16 +3,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        // StringExamples.MostRepeatedString("sansanababsafjsaasjfksdjfsfd", 2);
-        // StringExamples.DuplicateCharacterFromString("Common");
-        // StringExamples.UniqueCharactersFromString("Common");
+        StringExamples.MostRepeatedString("sansanababsafjsaasjfksdjfsfd", 2);
+        StringExamples.DuplicateCharacterFromString("Common");
+        StringExamples.UniqueCharactersFromString("Common");
 
-        // string result = StringExamples.ReverseString("sangam");
-        // Console.WriteLine("the reverse string is :" + result);
+        string result = StringExamples.ReverseString("sangam");
+        Console.WriteLine("the reverse string is :" + result);
 
-        // StringExamples.ReverseWords("my name is sangam");
-        // Console.WriteLine(StringExamples.IsStringPalindrome(""));
-        // StringExamples.MaxCharOccurance("my name is sangam");
+        StringExamples.ReverseWords("my name is sangam");
+        Console.WriteLine(StringExamples.IsStringPalindrome(""));
+        StringExamples.MaxCharOccurance("my name is sangam");
         StringExamples.GetPossibleSubString("Sangam");
     }
 }
